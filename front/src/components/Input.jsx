3797b529d5561c139379db1c269e108490d295ea
Thomas-Fch <thomas.fachinetti@gmail.com>
2023-06-29 17:18:57 +0200

    landing page done

--- front/src/components/Input.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-function Input({type, value = null, setValue = null}) {
-    return (
-        <input type={type} value={value} onChange={(e) => setValue(e.target.value)} />
-    )
-}
-
-export default Input;
